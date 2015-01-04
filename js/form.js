b8d2d0c530bb17b6e4417f9066797b61843e1f4d
Justin Yang <justin@y4ng.com>
2015-01-03 23:50:32 -0800

    Fixed form problems
    Added some cool transitions and corrected uploading problems with Formspree.

--- js/form.js
+++ js/form.js
@@ -1,36 +1,24 @@
-$(document).ready(function(){
+$( document ).ready(function(){
+  $('#signup').submit(function(){
+    if ( $('#signup').hasClass('valid')){
+      $( '#signup' ).addClass('sent');
+      $.ajax({
+        url: "//formspree.io/info@gunnhacks.com", 
+        method: "POST",
+        data: {message: $('.email').val()},
+        dataType: "json"
+      }).done(function( data ) {
+        });
 
-  function submitEmail(){
-     // Grab the email
-    var email = $('#email').val();
-
-    var $this = $(this);
-
-    $this.addClass('loading');
-
-    $.ajax({
-      url: "//formspree.io/info@gunnhacks.com", 
-      method: "POST",
-      data: {message: "hello!"},
-      dataType: "json"
-    }).done(function() {
-      $this.removeClass('loading');
-
-      $('#email').val('');
-
-      $('.hidden').show();
-
-    });
-  }
-
-  $('#submit').click(function(e){
-    submitEmail();
-  });
-
-  $('#email').keyup(function(e){
-    if (e.keyCode == 13) {
-      submitEmail();
+        //$('input[type="submit"]').val('Thank you! ♥ We\'ll email ' + $('.email').val() + ' when registration opens.');
+        $('input[type="submit"]').val('Thank you! ♥');
     }
-  });
+    
+        return false;
+    });
+});
 
+$('.email').keyup(function(){
+  var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    re.test(this.value)  ? $( '#signup' ).addClass( 'valid' ) : $( '#signup' ).removeClass( 'valid' );
 });
